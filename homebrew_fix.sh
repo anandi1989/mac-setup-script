d30c94473c74fe13125f1e44eabf29dc05b8bee3
Amit Nandi <amit_nandi@live.com>
2021-01-20 19:55:36 +0530

    Initial push

---
+++ homebrew_fix.sh
@@ -0,0 +1,6 @@
+rm -rf /usr/local/bin/brew
+rm -rf /usr/local/share/doc/homebrew
+rm -rf /usr/local/share/man/man1/brew.1
+rm -rf /usr/local/share/zsh/site-functions/_brew
+rm -rf /usr/local/etc/bash_completion.d/brew
+rm -rf /usr/local/Homebrew
